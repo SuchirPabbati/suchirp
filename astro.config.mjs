fc947488faf897ca358613f06df784ee585df736
“suchir” <suchirp14@gmail.com>
2025-09-12 18:41:37 -0400

    astro portfolio build fix v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: 'https://suchirpabbati.github.io',
+  base: '/suchirp',
   devToolbar: {
     enabled: false
   }
